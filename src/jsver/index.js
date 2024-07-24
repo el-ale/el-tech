c4e92b6d79f9214a75ffb4b4b031f6f574972e69
Pain-the-invuneravel <alejandro140220@gmail.com>
2024-07-24 12:09:43 -0300

    correção el tech estou fliz sendo pobre demais mas sobressaindo mais uma vez

---
+++ src/jsver/index.js
@@ -0,0 +1,16 @@
+console.log('index.js is loaded');
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed');
+    const app = document.getElementById('app');
+    if (app) {
+        app.innerText = 'Bem-vindo à Homepage!';
+    } else {
+        console.error('Element with id "app" not found');
+    }
+});
+
+console.log('index.js is loaded');
+
+document.getElementById('app').innerText = 'Bem-vindo à Homepage!';
+import '../templates/css/estilo.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,62 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
-  },
-  mode: 'development', // ou 'production' conforme necessário
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/templates/homepage.html', // Caminho para o seu arquivo HTML de template
-      filename: 'homepage.html', // Nome do arquivo de saída
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
+    mode: 'development',
+    entry: './src/jsver/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: 'html-loader',
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/templates/homepage.html',
+            filename: 'homepage.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/Notas.html',
+            filename: 'Notas.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/Sobre mim.html',
+            filename: 'Sobre mim.html',
+        }),
     ],
-  },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    stats: {
+        children: true,
+    },
 };
